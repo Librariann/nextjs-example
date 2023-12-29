d6e63c76986d6065dc9e1c50e13dbb0820c24684
Librariann <okpc0306@naver.com>
2023-12-29 21:18:11 +0900

    [commit] add post contents, getStaticPaths, getStaticProps

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,47 @@
-const Posts = () => {
-  return <div>[id]</div>;
+import { getAllPostIds, getPostData } from "@/lib/post";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+
+type postDataType = {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+};
+
+const Posts = ({ postData }: postDataType) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Posts;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering}}, {params: {id: 'ssg-ssr'}}]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
